59660bd1c00f3aded32ea14ac80ccc4f7dbc02c2
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-04-04 13:11:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const username = document.querySelector('#name');
+const local = document.querySelector('#local');
+const item = document.querySelector('#item');
+const itemValue = document.querySelector('#value');
+
+
+const recordValue = (event) => {
+    event.preventDefault();
+
+
+    const data = {
+        data: [
+            {
+                'id': "INCREMENT",
+                'nome': username.value,
+                'aonde foi?': local.value,
+                'o que foi?': item.value, 
+                'valor': itemValue.value,
+                'data': "DATETIME"
+            }
+        ]
+    };
+
+    fetch('https://sheetdb.io/api/v1/eat6lwcbrd5yp', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+document.querySelector('#main-form').addEventListener('submit', recordValue)
